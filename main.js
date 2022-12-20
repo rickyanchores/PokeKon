472c8acd4c12300a47cb9021ccea19af743181f5
RickyAnchores <ricky23anchores@gmail.com>
2022-12-20 18:12:00 +0000

    added result

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+let result = document.querySelector(".result")
+let btn = document.querySelector(".btn")
+
 async function getPoke(){
     let url = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
     fetch(url)
         .then((res) => res.json())
-        .then((data) {
+        .then((data) => {
             console.log(data)
+            result.innerHTML = `<h1>${data.results[1].name}</h1>`
         })
 }
 
+btn.addEventListener("click",getPoke)
 
-getPoke();
