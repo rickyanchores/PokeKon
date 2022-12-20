0b995a887e80646956522ba42843766e02ab1735
RickyAnchores <ricky23anchores@gmail.com>
2022-12-20 23:13:03 +0000

    added image and name

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 let result = document.querySelector(".result")
+let name = document.querySelector(".name")
+let imageBox = document.querySelector(".imageBox")
 let btn = document.querySelector(".btn")
 
-async function getPoke(){
-    let url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+async function getPoke(pokename){
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokename}`
 
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
             console.log(data)
-            result.innerHTML = `<h1>${data.results[1].name}</h1>`
+            name.innerHTML = `<h1>${data.name}</h1>`
+            imageBox.innerHTML = `<img src=${data.sprites.front_default} />`
         })
 }
 
-btn.addEventListener("click",getPoke)
 
+btn.addEventListener("click",getPoke(prompt("insert pokename")))
