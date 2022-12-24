fb29f807a7106e3d9592f3f2b0d405a22831fba9
RickyAnchores <ricky23anchores@gmail.com>
2022-12-24 21:04:23 +0000

    added weight and height

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let result = document.querySelector(".result")
 let name = document.querySelector(".name")
 let imageBox = document.querySelector(".imageBox")
+let height = document.querySelector(".height")
+let weight = document.querySelector(".weight")
 let btn = document.querySelector(".btn")
 
 async function getPoke(pokename){
@@ -12,6 +14,8 @@ async function getPoke(pokename){
             console.log(data)
             name.innerHTML = `<h1>${data.name}</h1>`
             imageBox.innerHTML = `<img src=${data.sprites.front_default} />`
+            weight.innerHTML = `<h3>Weight: ${data.weight}</h3>`
+            height.innerHTML = `<h3>Height: ${data.height}</h3>`
         })
 }
 
