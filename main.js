ad396aa01aa7823a3b1d3cb98b5f579f6a516bd3
RickyAnchores <ricky23anchores@gmail.com>
2023-01-04 12:24:31 +0100

    edit

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ async function getPoke(pokename){
             imageBox.innerHTML = `<img src=${data.sprites.front_default} />`
             weight.innerHTML = `<h3>Weight: ${data.weight}</h3>`
             height.innerHTML = `<h3>Height: ${data.height}</h3>`
+            moves.innerHTML = `<p> ${data.moves[17].move.name}</p>`
+
         })
 
 }
